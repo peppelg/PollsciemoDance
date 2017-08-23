c213a711e40c20efff3e479eba552241628d0401
peppelg <peppelg@bmtrad.it>
2017-08-23 11:22:04 +0200

    we is hacking

---
+++ pollsciemo.js
@@ -0,0 +1,26 @@
+function merde() {
+  setInterval(function () {
+    $('html').css("background-color", "blue")
+    setTimeout(function () {
+      $('html').css("background-color", "red")
+    }, 50)
+  }, 100)
+}
+var wharz = 1;
+setTimeout(function() {
+  $('#smerde').text('We is chiamando Pollsciemo...')
+  wharz = setInterval(function(){
+    $('#pollsciemi').prepend('<img src="pollsciemo.png" height=200 width=200>')
+  }, 500)
+}, 3000)
+setTimeout(function() {
+  $('#smerde').text('Jes, u ar owned by Pollsciemo...')
+}, 10000)
+setTimeout(function() {
+  $('#smerde').text('Habballiam??')
+}, 15000)
+setTimeout(function() {
+  clearTimeout(wharz)
+  $('img').attr('class','OPSE');
+  merde()
+}, 18000)
